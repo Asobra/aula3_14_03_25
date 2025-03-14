31d1676d2ad2af68767f79fcdfc3eac832fff5ce
Asobra <elias.sousa.barbosa0@gmail.com>
2025-03-14 23:34:32 +0000

    editando_style_novo.tsx

--- app/novo.tsx
+++ app/novo.tsx
@@ -39,12 +39,13 @@ export default function NovoContato() {
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, padding: 20 },
+    container: { flex: 1, padding: 16 },
     contato: { flexDirection: "row", padding: 10, borderBottomWidth: 1 },
     foto: { width: 50, height: 50, borderRadius: 25 },
     semFoto: { width: 50, height: 50, justifyContent: "center", alignItems: "center", backgroundColor: "#ddd", borderRadius: 25 },
     info: { marginLeft: 10 },
     nome: { fontWeight: "bold" },
+    input: { height: 40, borderColor: "blue", borderWidth: 1, marginBottom: 12, paddingHorizontal:8 },
     botaoAdicionar: { backgroundColor: "blue", padding: 10, marginTop: 20, borderRadius: 5, alignItems: "center" },
     textoBotao: { color: "white", fontSize: 16 },
   });
